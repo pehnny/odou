d2f5b596931b42c045315de74e443680710225db
Theo <theo.piron.pro@gmail.com>
2026-06-15 15:51:25 +0200

    Add(init): configure exports for module model

---
+++ model/__init__.py
@@ -0,0 +1,16 @@
+from .Employee import Employee
+from .Team import Team
+from .Role import Role
+from .Schedule import Schedule
+from .Mission import Mission
+from .Client import Client
+
+__all__ = [
+    "Employee",
+    "Team",
+    "Role",
+    "Schedule",
+    "Mission",
+    "Client",
+    
+]
