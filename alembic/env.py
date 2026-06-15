63290875fa8164d4a62fb648ba625a91a62746eb
Theo <theo.piron.pro@gmail.com>
2026-06-15 16:30:19 +0200

    Change(migration): rename migration folder to alembic

--- alembic/env.py
+++ alembic/env.py
@@ -5,9 +5,21 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from sqlmodel import SQLModel
+from model import *
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+_database_url = os.getenv("URL")
+if _database_url == None:
+    raise ValueError()
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", _database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +30,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
