0db70b4cb0d3795ffe54cfd0a6d2d269191ba937
Theo <theo.piron.pro@gmail.com>
2026-06-16 15:35:34 +0200

    Change(engine): configure engine for schema

--- database/config.py
+++ database/config.py
@@ -4,4 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 _dabatabase_url = os.getenv("URL")
-engine = create_engine(_dabatabase_url)
+engine = create_engine(
+    _dabatabase_url,
+    echo=True,
+    connect_args={"options": f"-csearch_path={os.getenv("POSTGRES_SCHEMA")}"}
+)
